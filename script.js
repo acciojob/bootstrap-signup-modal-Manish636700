11c9e82cff6070182646fc7bab332f036abf68b7
Manish Kumar Mittal <98324095+Manish636700@users.noreply.github.com>
2025-01-01 20:27:21 +0530

    created a submission

--- script.js
+++
@@ -1,4 +0,0 @@
-cy.get('#signupModal').should('be.visible', { timeout: 6000 });
-cy.get('button[data-bs-target="#signupModal"]').click();
-cy.wait(500); // Wait for modal animation
-cy.get('#signupModal').should('be.visible');
