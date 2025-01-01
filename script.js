456cd8c5a593efd1d973b438e202d59fbd549f81
Manish Kumar Mittal <98324095+Manish636700@users.noreply.github.com>
2025-01-01 17:33:36 +0530

    created a submission

--- script.js
+++
@@ -1,6 +0,0 @@
-it('closes the modal when the close button is clicked', () => {
-  cy.get('button[data-bs-toggle="modal"]').click(); // Open the modal
-  cy.get('.modal').should('be.visible'); // Ensure the modal is visible
-  cy.get('.btn-close').click(); // Click the close button
-  cy.get('.modal').should('not.be.visible'); // Ensure the modal is closed
-});
