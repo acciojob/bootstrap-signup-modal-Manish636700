6daa71148b45925121da1ef325baca87d5640b61
Manish Kumar Mittal <98324095+Manish636700@users.noreply.github.com>
2025-01-01 21:40:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-cy.get('#signupModal').should('not.be.visible'); // Assert it is not visible before clicking
-cy.get('button[data-bs-toggle="modal"]').click(); // Click to trigger the modal
-cy.get('#signupModal', {timeout: 10000}).should('be.visible'); // Wait for the modal to be visible
+describe('Signup Modal', () => {
+  it('should open and close the signup modal', () => {
+    // Visit the page
+    cy.visit('your_page_url');
+
+    // Assert that the modal is initially hidden
+    cy.get('#signupModal').should('not.be.visible');
+
+    // Trigger the modal to open
+    cy.get('button[data-bs-toggle="modal"]').click();
+
+    // Assert that the modal is visible after clicking the button
+    cy.get('#signupModal').should('be.visible');
+
+    // Close the modal
+    cy.get('.btn-close').click();
+
+    // Assert that the modal is no longer visible
+    cy.get('#signupModal').should('not.be.visible');
+  });
+});
